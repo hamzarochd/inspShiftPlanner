d6d43299f472c40ea894c883df5741bbdd547d50
Hamza Rochd <92557439+HaRo97@users.noreply.github.com>
2026-03-25 16:57:05 +0100

    planning calendar structure done

--- app/inps_shiftplanner/webapp/controller/viewPianificazoineTurni.controller.js
+++ app/inps_shiftplanner/webapp/controller/viewPianificazoineTurni.controller.js
@@ -1,10 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("inpsshiftplanner.controller.viewPianificazoineTurni", {
         onInit() {
+            // Carichiamo dati da un file JSON
+            const oModel = new JSONModel();
+            oModel.loadData("model/mockdata.json").then(() => {
+                // Una volta caricati i dati, aggiorniamo il modello
+                oModel.refresh();
+            });
+            
+            // Lo settiamo come modello della view
+            this.getView().setModel(oModel, "mockdata");
         }
     });
 });
